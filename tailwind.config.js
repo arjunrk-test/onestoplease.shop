225437429762e5e0c08a498c8e6cc052b1377b04
Arjun Saravanakumar <arjunrk.testing@gmail.com>
2025-05-06 14:43:58 +0530

    Initial commit with required dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+const tailwindcssAnimate = require('tailwindcss-animate');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrains-mono)",
+    },
+    extend: {
+      colors: {
+        accent: "#EDAD2D",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        gray: "var(--gray)",
+        highlight: "var(--highlight)",
+        grayInverted: "var(--gray-inverted)"
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate, require("tailwind-scrollbar-hide"),],
+};
