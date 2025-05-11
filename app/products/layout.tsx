4a143ba294989e74ee7f1600849608843c8f7f79
Arjun Saravanakumar <arjunrk.testing@gmail.com>
2025-05-11 18:44:31 +0530

    Added main products page

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,14 +1,17 @@
-// app/products/layout.tsx
-import Navbar from "@/components/Navbar";  
-import PagesNav from "@/components/PagesNav"; 
+'use client';
+import Navbar from "@/components/Navbar";
+import PagesNav from "@/components/PagesNav";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hidePagesNav = pathname === "/products"; 
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      {/* <div className="h-4" /> */}
-      <PagesNav />
+      {!hidePagesNav && <PagesNav />}
       <main className="flex-1">{children}</main>
     </div>
   );
---
+++ app/products/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { CategoriesPath } from "@/app/constants";
+
+function getDescriptionForCategory(name: string) {
+   switch (name.toLowerCase()) {
+      case "furniture":
+         return "Beds, sofas, tables & more";
+      case "appliances":
+         return "Fridge, oven, washing machine";
+      case "electronics":
+         return "Mobiles, laptops & gadgets";
+      case "vehicles":
+         return "Scooters, bikes, cars";
+      case "fitness":
+         return "Treadmills, bikes & more";
+      case "baby":
+         return "Cribs, toys & essentials";
+      default:
+         return "";
+   }
+}
+
+
+export default function ProductsPage() {
+   return (
+      <main className="min-h-screen bg-background text-foreground px-6 py-10">
+         <h1 className="text-3xl font-bold mb-8 text-center">Explore Our Categories</h1>
+
+         <div className="grid grid-cols-2 gap-6 max-w-6xl mx-auto">
+            {CategoriesPath.map(({ name, icon, pathName }) => (
+               <Link key={pathName} href={pathName}>
+                  <div className="bg-foreground text-background dark:bg-muted rounded-xl p-5 cursor-pointer transition-all group flex flex-col items-center text-center shadow hover:shadow-xl hover:shadow-highlight">
+                     <div className="text-4xl mb-3 text-highlight group-hover:scale-110 group-hover:text-accent  transition-transform">
+                        {icon}
+                     </div>
+                     <p className="text-base font-semibold text-background">{name}</p>
+                     <p className="text-xs text-muted-foreground mt-1">
+                        {getDescriptionForCategory(name)}
+                     </p>
+                  </div>
+               </Link>
+
+            ))}
+         </div>
+      </main>
+   );
+}
